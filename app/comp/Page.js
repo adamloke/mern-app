9a43ef30d7bf269a4dba9bb7b6d0fb24f3783305
Adam Loke <adam.loke@hyperisland.se>
2020-08-30 20:08:13 +0200

    display post content at single post page. fixed linking and page title.

--- app/comp/Page.js
+++ app/comp/Page.js
@@ -1,16 +1,12 @@
 import React, { useEffect } from "react"
-import Container from './Container'
+import Container from "./Container"
 
 function Page(props) {
   useEffect(() => {
     document.title = `${props.title} | App`
-    window.scrollTo
-  }, [])
-  return (
-    <Container wide={props.wide}>
-      {props.children}
-    </Container>
-  )
+    window.scrollTo(0, 0)
+  }, [props.title])
+  return <Container wide={props.wide}>{props.children}</Container>
 }
 
 export default Page
